e6babca1b9245984498df07d00d0884073831099
Scott Messinger <scottmessinger@gmail.com>
2015-06-05 10:05:29 -0400

    cleanup cli files

--- cli/000_asn_overview_converter.rb
+++ cli/000_asn_overview_converter.rb
@@ -19,43 +19,17 @@ Oj.default_options = {
 }
 
 
-docs = Oj.load(File.read('sources/asn_standards_documents.js'))
-
+docs          = Oj.load(File.read('sources/asn_standards_documents.js'))
 client        = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'standards')
 jurisdictions = client["jurisdictions"]
 find_id       = lambda{ |title| jurisdictions.find({title: title}).to_a.first[:_id]}
+hydra         = Typhoeus::Hydra.new(max_concurrency: 20)
 
-new_docs = docs["documents"].map{|doc|
-  {
-    date_modified:   doc["data"]["date_modified"][0],
-    date_valid:      doc["data"]["date_valid"][0],
-    description:     doc["data"]["description"][0],
-    id:              doc["id"].upcase,
-    jurisdiction:    doc["data"]["jurisdiction"][0],
-    jurisdiction_id: find_id.call(doc["data"]["jurisdiction"][0]),
-    subject:         SOURCE_TO_SUBJECT_MAPPINGS[doc["data"]["title"][0]],
-    title:           doc["data"]["title"][0],
-    url:             doc["data"]["identifier"][0],
-  }
-}.map{|doc|
-  doc[:url]
-}
-
-# p new_docs[0] + "_full.json"
-# response = Typhoeus.get(new_docs[0] + "_full.json", followlocation: true)
-# doc = ASNResourceParser.convert(Oj.load(response.body))
-# client[:standards_documents].find({_id: doc["_id"]}).update_one(doc, {upsert: true})
-
-
-taken = new_docs#[0..1]
-hydra = Typhoeus::Hydra.new(max_concurrency: 20)
-
-requests = taken.map{ |url|
-  request = Typhoeus::Request.new(url + "_full.json", followlocation: true)
+requests = docs["documents"].map{ |doc|
+  request = Typhoeus::Request.new(doc[:url] + "_full.json", followlocation: true)
   hydra.queue(request)
   request
 }
-
 
 hydra.run
 
