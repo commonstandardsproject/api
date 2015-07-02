055771b22a3d8b76cce82323fdebd66070159c08
Scott Messinger <scottmessinger@gmail.com>
2015-07-02 03:08:59 -0400

    Added back important bit of code that I accidentally took out earlier

--- cli/000_asn_overview_converter.rb
+++ cli/000_asn_overview_converter.rb
@@ -25,7 +25,22 @@ jurisdictions = client["jurisdictions"]
 find_id       = lambda{ |title| jurisdictions.find({title: title}).to_a.first[:_id]}
 hydra         = Typhoeus::Hydra.new(max_concurrency: 20)
 
-requests = docs["documents"].map{ |doc|
+
+new_docs = docs["documents"].map{|doc|
+  {
+    date_modified:   doc["data"]["date_modified"][0],
+    date_valid:      doc["data"]["date_valid"][0],
+    description:     doc["data"]["description"][0],
+    id:              doc["id"].upcase,
+    jurisdiction:    doc["data"]["jurisdiction"][0],
+    jurisdiction_id: find_id.call(doc["data"]["jurisdiction"][0]),
+    subject:         SOURCE_TO_SUBJECT_MAPPINGS[doc["data"]["title"][0]],
+    title:           doc["data"]["title"][0],
+    url:             doc["data"]["identifier"][0],
+  }
+}
+
+requests = new_docs["documents"].map{ |doc|
   request = Typhoeus::Request.new(doc[:url] + "_full.json", followlocation: true)
   hydra.queue(request)
   request
