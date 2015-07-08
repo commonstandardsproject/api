8258d1ec318004be05b58a80ba4573ecc0b9d9b4
Scott Messinger <scottmessinger@gmail.com>
2015-07-08 12:41:15 -0400

    Change pool size

--- config/mongo.rb
+++ config/mongo.rb
@@ -4,4 +4,4 @@ logger = Logger.new(STDOUT)
 logger.level = Logger::DEBUG
 Mongo::Logger.logger = logger
 connection_string = ENV["MONGODB_CONNECTION_STRING"] || [ '127.0.0.1:27017' ]
-$db = $db || Mongo::Client.new(connection_string, :database => 'common-standards-project', :max_pool_size => 32, :min_pool_size => 16)
+$db = $db || Mongo::Client.new(connection_string, :database => 'common-standards-project', :max_pool_size => 16)
