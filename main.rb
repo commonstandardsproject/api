643871728acb662fd2ac42a89e13f9eed372bc53
Scott Messinger <scottmessinger@gmail.com>
2015-07-08 22:53:28 -0400

    Add a sitemap.xml endpoint
    As I note in the comment, this isn’t ideal. If I take the time to
    customize the routing on the fronted, I’ll gladly move this to the
    sinatra main.rb file.

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 require 'logger'
-require 'nokogiri'
 
 class Main < Sinatra::Base
 
@@ -8,23 +7,9 @@ class Main < Sinatra::Base
   end
 
 
+  # Renders the swagger api docs
   get '/' do
     File.read(File.join('public', 'index.html'))
-  end
-
-  get '/sitemap.xml' do
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.urlset("xmlns" => "http://www.sitemaps.org/schemas/sitemap/0.9") {
-        $db[:standard_sets].find().projection({_id: 1}).batch_size(1000).map{|doc|
-          xml.url{
-            xml.loc "http://beta.commonstandardsproject.com/search?ids=%5B\"#{doc["_id"]}\"%5D"
-          }
-        }
-      }
-    end
-
-    content_type "text/xml"
-    builder.to_xml
   end
 
 
