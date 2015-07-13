40e6b3c03de30a2b043ed3539f34dbff78cd6dd5
Scott Messinger <scottmessinger@gmail.com>
2015-07-13 11:49:05 -0400

    Add a version to the API

--- api/entities/standard_set_summary.rb
+++ api/entities/standard_set_summary.rb
@@ -4,10 +4,33 @@ require 'grape-entity'
 module API
   module Entities
     class StandardSetSummary < Grape::Entity
-      expose :_id, as: :id
+      expose :_id, as: :id, documentation: {desc: "ID"}
       expose :title, documentation: {desc: "The name of the set"}
       expose :subject, documentation: {desc: "The subject"}
-      expose :educationLevels, documentation: {desc: "The education levels", is_array: true}
+      expose :educationLevels, documentation: {desc: "The education levels", values: [
+        "Pre-K",
+        "K",
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+        "VocationalTraining",
+        "ProfessionalEducation-Development",
+        "Graduate",
+        "HigherEducation",
+        "Undergraduate-UpperDivision",
+        "Undergraduate-LowerDivision",
+        "AdultEducation",
+        "LifeLongLearning",
+      ]}
       expose :document
       # expose :documentTitle
     end
