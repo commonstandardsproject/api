4620cdc080bc43cd9f4f1c9c1ffcebdb744a76d4
Scott Messinger <scottmessinger@gmail.com>
2016-05-06 12:04:46 -0400

    return id with standard set

--- api/standard_sets.rb
+++ api/standard_sets.rb
@@ -23,6 +23,7 @@ module API
         # Add the ancestor ids to the response. This a read only field
         standard_set["standards"] = StandardHierarchy.add_ancestor_ids(standard_set["standards"])
         standard_set["educationLevels"] ||= []
+        standard_set["id"] = standard_set["_id"]
 
         present :data, standard_set, with: Entities::StandardSet
       end
