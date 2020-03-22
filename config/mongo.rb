bd800e18c890646c538b8b0f481f3f2eeeae62f8
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-03-21 23:21:34 -0400

    Add auth source

--- config/mongo.rb
+++ config/mongo.rb
@@ -3,4 +3,4 @@ require 'mongo'
 
 Mongo::Logger.logger.level = Logger::WARN
 connection_string = ENV["MONGODB_CONNECTION_STRING"] || [ '127.0.0.1:27017' ]
-$db = $db || Mongo::Client.new(connection_string, auth_mech: :scram, :database => ENV["MONGODB_DATABASE"])
+$db = $db || Mongo::Client.new(connection_string, auth_mech: :scram, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"])
