f2d8cfb7e6c2b6b9f30c4020a47198f77e26b1e6
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-03-21 22:54:26 -0400

    bump

--- config/mongo.rb
+++ config/mongo.rb
@@ -1,5 +1,6 @@
 require 'mongo'
 
+
 Mongo::Logger.logger.level = Logger::WARN
 connection_string = ENV["MONGODB_CONNECTION_STRING"] || [ '127.0.0.1:27017' ]
 $db = $db || Mongo::Client.new(connection_string, :database => ENV["MONGODB_DATABASE"])
