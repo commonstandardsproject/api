54d2a447642604f8ff032e3cad88728ea01e1f7e
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-03-21 23:25:02 -0400

    whitespace

--- config/mongo.rb
+++ config/mongo.rb
@@ -1,6 +1,5 @@
 require 'mongo'
 
-
 Mongo::Logger.logger.level = Logger::WARN
 connection_string = ENV["MONGODB_CONNECTION_STRING"] || [ '127.0.0.1:27017' ]
 $db = $db || Mongo::Client.new(connection_string, auth_mech: :scram, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"])
