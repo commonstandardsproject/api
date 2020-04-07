b3827a7764e9f97a94889dfb3df86d07d74f5d08
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-04-07 10:25:45 -0400

    Set max pool size on Mongo

--- config/mongo.rb
+++ config/mongo.rb
@@ -2,4 +2,4 @@ require 'mongo'
 
 Mongo::Logger.logger.level = Logger::WARN
 connection_string = ENV["MONGODB_CONNECTION_STRING"] || [ '127.0.0.1:27017' ]
-$db = $db || Mongo::Client.new(connection_string, auth_mech: :scram, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"])
+$db = $db || Mongo::Client.new(connection_string, auth_mech: :scram, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"], max_pool_size: 8)
