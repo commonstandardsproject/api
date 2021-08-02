86e6ddbed1f8a7cfa16a43662e2e857e30fa8e04
Scott Ames-Messinger <scott@commoncurriculum.com>
2021-08-01 21:14:13 -0400

    ugh. typo

--- config/mongo.rb
+++ config/mongo.rb
@@ -3,7 +3,7 @@ require 'mongo'
 Mongo::Logger.logger.level = Logger::WARN
 connection_string = ENV["MONGODB_CONNECTION_STRING"] || [ '127.0.0.1:27017' ]
 if ENV['RACK_ENV'] == "development"
-  $db = $db || Mongo::Client.new(connection_string, auth_mech: auth_mech, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"], max_pool_size: 8, :direct_connection => true, :user => "admin", :password => "admin")
+  $db = $db || Mongo::Client.new(connection_string, auth_mech: :scram, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"], max_pool_size: 8, :direct_connection => true, :user => "admin", :password => "admin")
 else
-  $db = $db || Mongo::Client.new(connection_string, auth_mech: auth_mech, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"], max_pool_size: 8)
+  $db = $db || Mongo::Client.new(connection_string, auth_mech: :scram, auth_source: 'admin', :database => ENV["MONGODB_DATABASE"], max_pool_size: 8)
 end
