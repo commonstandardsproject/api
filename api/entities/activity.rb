36bd574643b8ba9a4d7e33085a799a8ae8260b11
Scott Ames-Messinger <scott@commoncurriculum.com>
2021-08-06 15:04:40 -0400

    Correctly serialize activities

---
+++ api/entities/activity.rb
@@ -0,0 +1,17 @@
+require 'bundler/setup'
+require 'grape-entity'
+
+module API
+  module Entities
+    class Activity < Grape::Entity
+      expose :id
+      expose :createdAt
+      expose :type,
+      expose :status
+      expose :title
+      expose :userId
+      expose :userName
+
+    end
+  end
+end
